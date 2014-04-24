29dd1e471f23e353ac41ef7195e2139eabcceccb
josh <joshbothun@gmail.com>
2014-04-23 23:43:00 -0700

    simplify view code

--- server.py
+++ server.py
@@ -6,6 +6,21 @@ from program import event_counter
 app = flask.Flask(__name__)
 
 
+# Default parameter values for solutions
+DEFAULT_PARAMS = [
+  # Solution 1: All events in 100 mile radius of Seattle, WA.
+  {'origin_latitude': 47.6097, 'origin_longitude': -122.3331, 'radius': 100},
+  # Solution 2: All events.
+  {},
+  # Solution 3: All events that occurred within the longtitude range -75 to -150
+  {'start_longitude': -150, 'end_longitude': -75, 'circular_query': False},
+  # Solution 3: All events of magnitude > 7.5
+  {'threshold': 7.5},
+  # Solution 5: All events in 100 mile radius of Seattle, WA.
+  {'origin_latitude': 47.6097, 'origin_longitude': -122.3331, 'radius': 100},
+]
+
+
 def build_parameters(request_args, origin_latitude=None, origin_longitude=None, 
                      radius=None, start_latitude=None, start_longitude=None,
                      end_latitude=None, end_longitude=None,
@@ -57,30 +72,22 @@ def validate_parameters(params):
 
 @app.route('/')
 def home():
-  return flask.redirect(flask.url_for('solution1'))
-
-
-@app.route('/1/')
-def solution1():
-  # Default input: All events in 100 mile radius of Seattle, WA.
-  params = build_parameters(
-      flask.request.args,
-      origin_latitude=47.6097, origin_longitude=-122.3331, radius=100)
-
-  # Validate input and process events if valid.
-  error = validate_parameters(params)
-  events = event_counter.get_events(**params) if not error else []
-
-  return flask.render_template(
-      'solution1.html', events=events, params=params, error=error)
+  return flask.redirect(flask.url_for('solution', index=1))
 
 
 @app.route('/<int:index>/')
 def solution(index):
+  params = build_parameters(flask.request.args, **DEFAULT_PARAMS[index - 1])
+  error = validate_parameters(params)
+  events = event_counter.get_events(**params) if not error else []
+
+  # Load view for solution
   try:
-    return flask.render_template('solution%d.html' % index)
+    return flask.render_template(
+      'solution%d.html' % index, events=events, params=params, error=error)
   except jinja2.exceptions.TemplateNotFound:
     return flask.abort(404)
+
 
 if __name__ == '__main__':
   # TODO: no debug for production
