66c48275ef867b25748bfc1e151edca7f70960d1
josh <joshbothun@gmail.com>
2014-04-24 18:12:20 -0700

    improve temporal plot

--- static/solution4.js
+++ static/solution4.js
@@ -1,40 +1,92 @@
 // Timeline plot of event data using d3.js
 var WIDTH = 800;
 var HEIGHT = 600;
-var STARTYEAR = 1900;
-var ENDYEAR = 2008;
 
 
 // Export init function
 window.initSolution = function(events) {
-  plotEvents(events);
+  // Merge year data
+  var yearData = mergeYearData(events);
+  plotYearData(yearData);
 };
 
 
-/* Create the chart layout and plot events */
-var plotEvents = function(events) {
+/* Create the chart layout and plot year data */
+var plotYearData = function(yearData) {
   // Create SVG element
   var svg = d3.select("#wrap").append("svg")
       .attr("width", WIDTH)
       .attr("height", HEIGHT);
 
-  svg.selectAll('.event').data(events).enter()
-      .append('circle')
-      .attr('class', 'event')
-      .attr('cx', transform_x)
-      .attr('cy', transform_y)
-      .attr('r', function(ev) {
-        return ev.m;
-      });
-}
+  var padding = 50;
+  var width = WIDTH - padding;
+  var height = HEIGHT - padding;
+
+  // Create axes
+  var years = _.pluck(yearData, 'year')
+  var x = d3.scale.linear()
+      .domain([_.min(years), _.max(years)])
+      .range([padding, width]);
+
+  var magnitudes = _.pluck(yearData, 'magnitude')
+  var y = d3.scale.linear()
+      .domain([_.min(magnitudes), _.max(magnitudes)])
+      .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+      .scale(x)
+      .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+      .scale(y)
+      .orient("left")
+      .ticks(10);
+
+  svg.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (height + 5) + ")")
+      .call(xAxis)
+    .append("text")
+
+  svg.append("g")
+      .attr("class", "y axis")
+      .attr("transform", "translate(" + padding +  ",0)")
+      .call(yAxis)
+    .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".71em")
+      .style("text-anchor", "end")
+      .text("Average magnitude")
+
+  // Plot events
+  svg.selectAll('.bar').data(yearData).enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(ev) { return x(ev.year) })
+      .attr('width', 5)
+      .attr('y', function(ev) { return y(ev.magnitude) })
+      .attr('height', function(ev) { return height - y(ev.magnitude) });
+};
 
 
-/* Coordinate transformation functions for charts */
-var transform_x = function(ev) {
-  var pos = (ev.y - STARTYEAR) / (ENDYEAR - STARTYEAR);
-  return pos * WIDTH;
-}
-var transform_y = function(ev) {
-  var pos = (ev.lat + 90) / 180;
-  return HEIGHT - pos * HEIGHT;
-}
+/* Calculate mean magnitudes for all years */
+var mergeYearData = function (events) {
+  console.log(events);
+  var years = {};
+  _.map(events, function(ev) {
+    years[ev.y] = years[ev.y] === undefined ? [] : years[ev.y]
+    years[ev.y].push(parseFloat(ev.m))
+  });
+  _.map(years, function(magnitudes, year) {
+    var sum = _.reduce(magnitudes, function(a, b) { return a + b; }, 0);
+    years[year] = sum / magnitudes.length;
+  });
+
+  return _.map(years, function(magnitude, year) {
+    return {
+      magnitude: magnitude,
+      year: parseInt(year)
+    }
+  });
+};
